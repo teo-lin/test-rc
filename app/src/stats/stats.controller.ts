492084022f76a2f877f99ba9d5a353e10ecaacc2
Teolin <teolin@gmail.com>
2023-08-30 15:27:19 +0300

    GitGuardian

--- app/src/stats/stats.controller.ts
+++ app/src/stats/stats.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StatsService } from './stats.service';
+import { Observable } from 'rxjs';
 
 @Controller('stats')
-export class StatsController {}
+export class StatsController {
+    constructor(private readonly statsService: StatsService) { }
+
+    @Get('temperatures')
+    getTemperatures(): Observable<any> {
+        return this.statsService.getTemperatures();
+    }
+}
--- app/src/stats/stats.service.ts
+++ app/src/stats/stats.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { WeatherService } from '../weather/weather.service';
+import { Observable, forkJoin, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+    constructor(private readonly weatherService: WeatherService) { }
+
+    getTemperatures(): Observable<any> {
+        const locations = ['Deva', 'Carei', 'Arad']; // Modify as needed
+
+        type temmperature = {
+            temperature: number
+        }
+
+        const stats: temmperature[] = locations.map(location =>
+            this.weatherService.getTemperature(location)
+        );
+
+        console.log(stats);
+
+        return forkJoin(stats).pipe(
+            map(responses => {
+                const temperatures = responses.map(response => {
+                    if (response && response.data && response.data.data[0]) {
+                        return response.data.data[0].temp;
+                    }
+                    return null; // Return null for invalid responses
+                });
+
+                const formattedData = locations.reduce((acc, location, index) => {
+                    acc[location] = temperatures[index];
+                    return acc;
+                }, {});
+
+                return formattedData;
+            })
+        );
+    }
+}
